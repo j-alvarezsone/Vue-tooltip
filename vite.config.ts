0c342726e0860c6a5467ca001636f042a1202bed
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2024-06-26 09:24:33 +0200

    chore: Remove unnecessary import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
