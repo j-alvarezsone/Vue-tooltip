b8d33f4921ad367d2fd93dc4148444ce17022a50
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2024-06-26 09:05:40 +0200

    chore: Update npm dependencies and add @rollup/plugin-dynamic-import-vars

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        dynamicImportVars({
+          // options for the plugin
+        }),
+      ],
     },
   },
 });
