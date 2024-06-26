08eb35a5ff978c60d9046172f79d3c6b86f9f116
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2024-06-26 14:35:05 +0200

    chore: Enable source maps and retain class/function names in production

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,22 @@ export default defineConfig({
     },
   },
   build: {
+    sourcemap: true, // Enable source maps for production
     rollupOptions: {
       plugins: [
         dynamicImportVars({
           // options for the plugin
         }),
       ],
+      output: {
+        sourcemap: true, // Ensure sourcemaps are enabled for all outputs
+      },
+    },
+    // Additional option to retain class names and function names in production for better debugging
+    minify: 'terser', // Use Terser for minification
+    terserOptions: {
+      keep_classnames: true,
+      keep_fnames: true,
     },
   },
 });
